wincent/ferret

commit dbf1b19e5da94116fbb479cfb739acada2a30894

Add g:FerretExecutable
Allow the user to force an override to their favorite executable.

This is just the initial dumb implementation. I'm actually going to
replace this with something smarter.

autoload/ferret/private.vim
@@ -353,18 +353,10 @@ function! ferret#private#lackcomplete(arglead, cmdline, cursorpos) abort
   return ferret#private#complete('Lack', a:arglead, a:cmdline, a:cursorpos, 1)
 endfunction
 
+" Return first word (the name of the binary) of the executable string.
 function! ferret#private#executable()
-  if executable('rg')
-    return 'rg'
-  elseif executable('ag')
-    return 'ag'
-  elseif executable('ack')
-    return 'ack'
-  elseif executable('grep')
-    return 'grep'
-  else
-    return ''
-  endif
+  let l:executable=FerretExecutable()
+  let l:binary=matchstr(l:executable, '\v\w+')
 endfunction
 
 let s:options = {

plugin/ferret.vim
@@ -440,14 +440,30 @@ let g:FerretLoaded = 1
 let s:cpoptions = &cpoptions
 set cpoptions&vim
 
+""
+" @option g:FerretExecutable string
+"
+" Ferret will preferentially use `rg`, `ag`, `ack` and finally `grep` (in that
+" order, using the first found executable), however you can force your
+" preference for a specific tool to be used by setting an override in your
+" |.vimrc|. Valid values are "rg", "ag", "ack" and "grep". If the requested
+" executable does not exist, Ferret will fall-back to the next in the list.
+"
+" Example:
+"
+" ```
+" let g:FerretExecutable='ag'
+" ```
+let s:force=get(g:, 'FerretExecutable', '')
+
 " Would ideally have these in an autoload file, but want to defer autoload
 " until as late as possible.
 function! FerretExecutable()
-  if executable('rg')
+  if (s:force == 'rg' || s:force == '') && executable('rg')
     return 'rg --vimgrep --no-heading'
-  elseif executable('ag')
+  elseif (s:force == 'ag' || s:force =='rg' || s:force == '') && executable('ag')
     return 'ag --vimgrep'
-  elseif executable('ack')
+  elseif (s:force == 'ack' || s:force == 'ag' || s:force == 'rg' || s:force == '') && executable('ack')
     return 'ack --column --with-filename'
   elseif executable('grep')
     let l:grepprg=&grepprg

commit 5a347fd06d8e3363731d32f06ac5f71bb1f888f3

Fix option autocompletion
Broken since dbf1b19e5da94116fbb479cfb739a.

autoload/ferret/private.vim
@@ -431,7 +431,7 @@ endfunction
 " Return first word (the name of the binary) of the executable string.
 function! ferret#private#executable_name()
   let l:executable=ferret#private#executable()
-  let l:binary=matchstr(l:executable, '\v\w+')
+  return matchstr(l:executable, '\v\w+')
 endfunction
 
 let s:options={

plugin/ferret.vim
@@ -357,7 +357,11 @@
 "
 " # History
 "
-" 4.0.2 (11 January 2019)
+" ## master (not yet released)
+"
+" - Fixed option autocompletion.
+"
+" ## 4.0.2 (11 January 2019)
 "
 " - Restore compatibility with versions of `rg` prior to v0.8
 "   (https://github.com/wincent/ferret/issues/59).